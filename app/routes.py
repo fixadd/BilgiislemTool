7cd27965985f2a10c43289ed56fb332068bf95db
kadir can avcı <61106929+fixadd@users.noreply.github.com>
2025-08-08 11:10:24 +0300

    Redirect unauthorized users to login

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///envanter.db'
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
