bb9cee70856720fc659e28c8a12830fbf8082ea7
kadir can avcı <61106929+fixadd@users.noreply.github.com>
2025-08-15 09:44:23 +0300

    Load session secret from environment

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
@@ -7,7 +9,12 @@ from routes import router as api_router
 from utils import cleanup_deleted
 
 app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key="super-secret-key")
+
+secret_key = os.getenv("SESSION_SECRET")
+if not secret_key:
+    raise RuntimeError("SESSION_SECRET environment variable is not set")
+
+app.add_middleware(SessionMiddleware, secret_key=secret_key)
 app.mount("/image", StaticFiles(directory="image"), name="image")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
